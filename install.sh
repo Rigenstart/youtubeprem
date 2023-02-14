78a2f90a439afc2806fa41e876066a8bf28754a3
Rigenstart <120707024+Rigenstart@users.noreply.github.com>
2023-02-14 20:11:14 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "Installing Perl Modules, Please wait..";
+pip install pycryptodome
+cpan install Net::IP
+cpan install Net::DNS::Resolver
+cpan install LWP::UserAgent
+cpan install HTTP::Request::Common
+cpan install WWW::Mechanize
+cpan install Term::ANSIColor
+echo "Perl Modules Installed Successfully. ✔";
